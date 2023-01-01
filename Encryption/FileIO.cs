934fac8e8f51af492093e8c5c23bf0ce8950031d
Moritakenjinosuke <unset>
2023-01-01 11:36:55 +0900

    プロジェクト ファイルを追加します。

---
+++ Encryption/FileIO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    internal class FileIO
+    {
+        //ファイルパス
+        private const string path = @"C:\Users\YHSHD\Desktop\pass.txt";
+
+        /// <summary>
+        /// ファイル書き出し
+        /// </summary>
+        /// <param name="text">書き出しテキスト</param>
+        /// <param name="path">ファイルパス</param>
+        public static void Write(string text,string path = path)
+        {
+            //上書きモードでファイル書き出し
+            using (var writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(text);
+            }
+        }
+
+        /// <summary>
+        /// ファイル読み込み
+        /// </summary>
+        /// <param name="path">ファイルパス</param>
+        /// <returns></returns>
+        public static string Read(string path = path)
+        {
+            using (var reader = new StreamReader(path))
+            {
+                var text = reader.ReadToEnd();
+                return text;
+            }
+        }
+
+    }
+}
---
+++ Encryption/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("パスワードを入力してください");
+            //共通暗号Key
+            Encryption.Key = Console.ReadLine();
+
+            Console.WriteLine("処理を選択してください");
+            Console.WriteLine("1:   暗号化");
+            Console.WriteLine("2:   復号");
+            var selected = Console.ReadLine();
+
+            switch (selected)
+            {
+                case "1":
+                    //平文
+                    string plainText = "passsw@rd";
+                    Console.WriteLine("plain: {0}", plainText);
+
+                    var str = Encryption.Encrypt(plainText);
+
+                    FileIO.Write(str);
+                    break;
+                case "2":
+                    //ファイルから読み出し
+                    string text = FileIO.Read();
+                    var decoded = Encryption.Decode(text);
+                    Console.WriteLine(decoded);
+
+                    break;
+                default:
+                    Console.WriteLine("正しいものを入力してください。");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
